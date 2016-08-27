f6d209f21851b682570d87809024bc5f701a52ce
patspeiser <pat@socialflow.com>
2016-08-27 00:38:24 -0400

    getting player data returned from db

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app = express();
 var swig = require('swig');
 var chalk = require('chalk');
 var GoogleMaps = require('googlemaps');
+var Action = require('./public/js/getData');
+var Models = require('./models');
+var Player = Models.Player;
+var Promise = require('bluebird');
 var Map = require('./models/maps')
 
 swig.setDefaults({ cached: false});
@@ -13,9 +17,18 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/node_modules/'));
 
 // root 
-app.get('/', function(req, res){
+app.get('/', function(req, res, next){
+	console.log(chalk.magenta('at /'))
 	res.render('index');
 });
+
+app.get('/test', function(req, res, next){
+	Promise.all([Player.findAll({})])
+	.then(function(data){
+		res.send('made it here', data)
+	})
+});
+
 
 //export app to mount on server
 module.exports = app;
